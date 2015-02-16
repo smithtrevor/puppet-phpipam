0b1bb26c6f99db8e1f487f163ab8423fe8cf730f
Trevor Smith <tsmith3@entertainment.com>
2015-02-16 15:02:19 -0500

    fix typo

--- manifests/install/apache.pp
+++ manifests/install/apache.pp
@@ -8,7 +8,7 @@ class phpipam::install::apache {
     server_root            => $::phpipam::apache_server_root,
     keepalive              => 'Off',
     max_keepalive_requests => '500',
-    keepalive_timeoute     => '15',
+    keepalive_timeout      => '15',
     mpm_module             => false,
     trace_enable           => 'Off',
     vhost_dir              => '/etc/httpd/vhosts.d',
