2cfbb24e251314c665085864744f3a3a3798a6ee
Trevor Smith <tsmith3@entertainment.com>
2015-02-16 17:26:52 -0500

    add initial vhost config

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,21 @@
 # This class is called from phpipam
 #
 class phpipam::config {
+
+  if $::phpipam::manage_apache {
+    
+    apache::vhost { 'phpipam' :
+      ip                => '*',
+      port              => '80',
+      servername        => $::phipam::site_fqdn,
+      ssl               => false,
+      ip_based          => false,
+      access_log_format => 'combined',
+      docroot           => "${::phpipam::apache_docroot}/phpipam",
+    }
+  }
+
+  if $::phpipam::manage_mysql {
+  }
+
 }
