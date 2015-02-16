5b504aef4cd907638f44de7d985a0ef41294cc10
Trevor Smith <tsmith3@entertainment.com>
2015-02-16 18:32:40 -0500

    change override to All to support prettified URLs

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,6 +14,7 @@ class phpipam::config {
       ip_based          => false,
       access_log_format => 'combined',
       docroot           => "${::phpipam::apache_docroot}/phpipam",
+      override          => ['All'],
     }
   }
 
