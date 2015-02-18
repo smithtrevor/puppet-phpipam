742621901bab0920a344cadb2d5abd36918ed1f9
Trevor Smith <tsmith3@entertainment.com>
2015-02-18 14:31:13 -0500

    fix typo in variable name and add logic to mysql.pp to prevent calling outside of the phpipam module

--- manifests/install/apache.pp
+++ manifests/install/apache.pp
@@ -1,5 +1,5 @@
 class phpipam::install::apache {
-  if $caller_module_name != $module {
+  if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
   
--- manifests/install/mysql.pp
+++ manifests/install/mysql.pp
@@ -1,4 +1,9 @@
 class phpipam::install::mysql {
+
+   if $caller_module_name != $module_name {
+      fail("Use of private class ${name} by ${caller_module_name}")
+    }
+
   class { '::mysql::server':
     remove_default_accounts => true,
     root_password           => $::phpipam::mysql_root_password,
